ac1d4ce20f1e9386fea3c1037245d9c9cd3257eb
Nathan Guerrero <naguerr2@uci.edu>
2025-12-02 14:43:50 +0000

    chore[bump]: Upgrade to NixOS 25.11
    Add extra step in flake-check workflow that checks whether the flake builds

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Basic NixOS setup with cloud-init, intended to be made into an ISO";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }: {
