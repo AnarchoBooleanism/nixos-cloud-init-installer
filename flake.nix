857ff972020ea9d4b2237c37f0152fb09a3c31a3
Nathan Guerrero <naguerr2@uci.edu>
2025-12-11 05:13:42 +0000

    fix: Update nixpkgs version for publish workflow, add comments on where to update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Basic NixOS setup with cloud-init, intended to be made into an ISO";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; # Change me on NixOS upgrades!
   };
 
   outputs = { self, nixpkgs }: {
